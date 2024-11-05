77d7de9e6e884dfde7b38d1679f6018b1083f1c0
marcschulz <marcleonschulz@gmail.com>
2024-11-05 10:14:14 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Content, Grid, Row, Column, Link } from "carbon-components-svelte";
+</script>
+<svelte:head>
+	<title>404</title>
+</svelte:head>
+<Content class="container">
+	<Grid>
+		<Row>
+			<Column>
+				<h1>404</h1>
+				<div>
+					Page not found.
+					<Link href="/">Return home</Link>
+				</div>
+			</Column>
+		</Row>
+	</Grid>
+</Content>
