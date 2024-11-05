55cb930dad88f8e0d071410988420847c14bdd6f
marcschulz <marcleonschulz@gmail.com>
2024-11-05 10:22:28 +0100

    add config

---
+++ src/lib/config.js
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Homepage'
+export const description = 'This is a homepage'
+export const url = dev ? 'http://localhost:5173/' : 'marc-schulz.online'
